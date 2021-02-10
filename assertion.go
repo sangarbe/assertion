3d6176d059fda3a5f7a39c419932d5449ad2a0ce
Santiago Garcia <sangarbe@gmail.com>
2021-02-10 08:23:03 +0100

    adds a generic comparison function and increases assertion api

--- assertion.go
+++ assertion.go
@@ -3,6 +3,7 @@ package assertion
 import (
 	"errors"
 	"fmt"
+	"reflect"
 )
 
 // Assertion represents a data assertion process. It provides several methods
@@ -49,9 +50,20 @@ func (a *Assertion) ErrorAt(index int) error {
 	return a.errors[len(a.errors)+index]
 }
 
-// appendError adds the default error message to Assertion or a formatted error
+// addError adds an error to Assertion
+func (a *Assertion) addError(err error) {
+	a.errors = append(a.errors, err)
+}
+
+// addErrorMsg adds the default error message to Assertion or a formatted error
 // message if msgArgs are provided
-func (a *Assertion) appendError(defaultMsg string, msgArgs ...interface{}) {
+func (a *Assertion) addErrorMsg(defaultMsg string, msgArgs ...interface{}) {
+	a.errors = append(a.errors, buildError(defaultMsg, msgArgs...))
+}
+
+// buildError returns an error with a default message or a message built from
+// message arguments if msgArgs are provided
+func buildError(defaultMsg string, msgArgs ...interface{}) error {
 	errMsg := defaultMsg
 	if len(msgArgs) == 1 {
 		errMsg = fmt.Sprintf("%+v", msgArgs[0])
@@ -61,5 +73,67 @@ func (a *Assertion) appendError(defaultMsg string, msgArgs ...interface{}) {
 		errMsg = fmt.Sprintf(msgArgs[0].(string), msgArgs[1:]...)
 	}
 
-	a.errors = append(a.errors, errors.New(errMsg))
+	return errors.New(errMsg)
+}
+
+const (
+	cmpOpEqual = iota
+	cmpOpNotEqual
+	cmpOpGreater
+	cmpOpLowerEqual
+	cmpOpLower
+	cmpOpGreaterEqual
+)
+
+var errMsgByOp = map[int]string{
+	cmpOpEqual:        errMsgNotEqual,
+	cmpOpNotEqual:     errMsgNotDifferent,
+	cmpOpGreater:      errMsgNotGreater,
+	cmpOpLowerEqual:   errMsgNotLowerEqual,
+	cmpOpLower:        errMsgNotLower,
+	cmpOpGreaterEqual: errMsgNotGreaterEqual,
+}
+
+// compare returns true if a given value and other operand satisfy the compare
+// operation determined by the operator. If the operation is not satisfied, this
+// function also returns an error (only comparable types allowed)
+func compare(op int, value, other interface{}, msgArgs ...interface{}) (bool, error) {
+	switch op {
+	case cmpOpNotEqual, cmpOpGreaterEqual, cmpOpLowerEqual:
+		ok, err := compare(op-1, value, other, msgArgs...)
+		if ok {
+			err = buildError(fmt.Sprintf(errMsgByOp[op], value, other), msgArgs...)
+		}
+		return !ok, err
+	}
+
+	switch value.(type) {
+	case bool:
+		v, o := value.(bool), other.(bool)
+		if op == cmpOpEqual && v == o {
+			return true, nil
+		}
+	case int, int8, int16, int32, int64:
+		v, o := reflect.ValueOf(value).Int(), reflect.ValueOf(other).Int()
+		if (op == cmpOpEqual && v == o) || (op == cmpOpGreater && v > o) || (op == cmpOpLower && v < o) {
+			return true, nil
+		}
+	case uint, uint8, uint16, uint32, uint64:
+		v, o := reflect.ValueOf(value).Uint(), reflect.ValueOf(other).Uint()
+		if (op == cmpOpEqual && v == o) || (op == cmpOpGreater && v > o) || (op == cmpOpLower && v < o) {
+			return true, nil
+		}
+	case float32, float64:
+		v, o := reflect.ValueOf(value).Float(), reflect.ValueOf(other).Float()
+		if (op == cmpOpEqual && v == o) || (op == cmpOpGreater && v > o) || (op == cmpOpLower && v < o) {
+			return true, nil
+		}
+	case string:
+		v, o := value.(string), other.(string)
+		if (op == cmpOpEqual && v == o) || (op == cmpOpGreater && v > o) || (op == cmpOpLower && v < o) {
+			return true, nil
+		}
+	}
+
+	return false, buildError(fmt.Sprintf(errMsgByOp[op], value, other), msgArgs...)
 }
--- assertion_methods.go
+++ assertion_methods.go
@@ -17,6 +17,7 @@ const (
 	errMsgNotLower        = `%v is not lower than %v`
 	errMsgNotGreaterEqual = `%v is not greater than or equal %v`
 	errMsgNotLowerEqual   = `%v is not lower than or equal %v`
+	errMsgNotDifferent    = `%v is not different %v`
 )
 
 var (
@@ -45,22 +46,32 @@ var (
 
 // EqualBool returns true if a given bool value is equal to other bool value
 func (a *Assertion) EqualBool(value, other bool, msgArgs ...interface{}) bool {
-	if other == value {
-		return true
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotEqual, value, other), msgArgs...)
-	return false
+	return ok
 }
 
 // True returns true if a given bool value is true
 func (a *Assertion) True(value bool, msgArgs ...interface{}) bool {
-	return a.EqualBool(value, true, msgArgs...)
+	ok, err := compare(cmpOpEqual, value, true, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // False returns true if a given bool value is false
 func (a *Assertion) False(value bool, msgArgs ...interface{}) bool {
-	return a.EqualBool(value, false, msgArgs...)
+	ok, err := compare(cmpOpEqual, value, false, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // Boolean returns true if a given string is one of the following accepted forms:
@@ -70,7 +81,7 @@ func (a *Assertion) Boolean(value string, msgArgs ...interface{}) bool {
 		return true
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotValid, value, "boolean string"), msgArgs...)
+	a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "boolean string"), msgArgs...)
 	return false
 }
 
@@ -80,7 +91,7 @@ func (a *Assertion) Integer(value string, msgArgs ...interface{}) bool {
 		return true
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotValid, value, "integer"), msgArgs...)
+	a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "integer"), msgArgs...)
 	return false
 }
 
@@ -91,7 +102,7 @@ func (a *Assertion) Truthy(value string, msgArgs ...interface{}) bool {
 		return b
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotValid, value, "truthy string"), msgArgs...)
+	a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "truthy string"), msgArgs...)
 	return false
 }
 
@@ -102,112 +113,187 @@ func (a *Assertion) Falsy(value string, msgArgs ...interface{}) bool {
 		return !b
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotValid, value, "falsy string"), msgArgs...)
+	a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "falsy string"), msgArgs...)
 	return false
 }
 
 // EqualInt64 returns true if a given int64 value is equal to other int64 value
 func (a *Assertion) EqualInt64(value, other int64, msgArgs ...interface{}) bool {
-	if other == value {
-		return true
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotEqual, value, other), msgArgs...)
-	return false
+	return ok
 }
 
 // EqualInt32 returns true if a given int32 value is equal to other int32 value
 func (a *Assertion) EqualInt32(value, other int32, msgArgs ...interface{}) bool {
-	return a.EqualInt64(int64(value), int64(other), msgArgs...)
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // EqualInt16 returns true if a given int16 value is equal to other int16 value
 func (a *Assertion) EqualInt16(value, other int16, msgArgs ...interface{}) bool {
-	return a.EqualInt64(int64(value), int64(other), msgArgs...)
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // EqualInt8 returns true if a given int8 value is equal to other int8 value
 func (a *Assertion) EqualInt8(value, other int8, msgArgs ...interface{}) bool {
-	return a.EqualInt64(int64(value), int64(other), msgArgs...)
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // EqualInt returns true if a given int value is equal to other int value
 func (a *Assertion) EqualInt(value, other int, msgArgs ...interface{}) bool {
-	return a.EqualInt64(int64(value), int64(other), msgArgs...)
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
+}
+
+// EqualUint64 returns true if a given uint64 value is equal to other uint64 value
+func (a *Assertion) EqualUint64(value, other uint64, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
+}
+
+// EqualUint32 returns true if a given uint32 value is equal to other uint32 value
+func (a *Assertion) EqualUint32(value, other uint32, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
+}
+
+// EqualUint16 returns true if a given uint16 value is equal to other uint16 value
+func (a *Assertion) EqualUint16(value, other uint16, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
+}
+
+// EqualUint8 returns true if a given uint8 value is equal to other uint8 value
+func (a *Assertion) EqualUint8(value, other uint8, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
+}
+
+// EqualUint returns true if a given uint value is equal to other uint value
+func (a *Assertion) EqualUint(value, other uint, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // EqualFloat64 returns true if a given float64 value is equal to other float64 value
 func (a *Assertion) EqualFloat64(value, other float64, msgArgs ...interface{}) bool {
-	if other == value {
-		return true
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotEqual, value, other), msgArgs...)
-	return false
+	return ok
 }
 
 // EqualFloat32 returns true if a given float32 value is equal to other float32 value
 func (a *Assertion) EqualFloat32(value, other float32, msgArgs ...interface{}) bool {
-	return a.EqualFloat64(float64(value), float64(other), msgArgs...)
-}
-
-// GreaterThanInt returns true if a given int value is greater than a min value
-func (a *Assertion) GreaterThanInt(value, min int64, msgArgs ...interface{}) bool {
-	if value > min {
-		return true
+	ok, err := compare(cmpOpEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotGreater, value, min), msgArgs...)
-	return false
+	return ok
+}
+
+// GreaterThanInt returns true if a given int value is greater than other int value
+func (a *Assertion) GreaterThanInt(value, other int64, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpGreater, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
+	}
+
+	return ok
 }
 
 // GreaterThanOrEqualInt returns true if a given int value is greater than or equal to a min value
-func (a *Assertion) GreaterThanOrEqualInt(value, min int64, msgArgs ...interface{}) bool {
-	if value >= min {
-		return true
+func (a *Assertion) GreaterThanOrEqualInt(value, other int64, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpGreaterEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotGreaterEqual, value, min), msgArgs...)
-	return false
+	return ok
 }
 
 // LowerThanInt returns true if a given int value is lower than a max value
-func (a *Assertion) LowerThanInt(value, max int64, msgArgs ...interface{}) bool {
-	if value < max {
-		return true
+func (a *Assertion) LowerThanInt(value, other int64, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpLower, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotLower, value, max), msgArgs...)
-	return false
+	return ok
 }
 
 // LowerThanOrEqualInt returns true if a given int value is lower than or equal to a min value
-func (a *Assertion) LowerThanOrEqualInt(value, max int64, msgArgs ...interface{}) bool {
-	if value <= max {
-		return true
+func (a *Assertion) LowerThanOrEqualInt(value, other int64, msgArgs ...interface{}) bool {
+	ok, err := compare(cmpOpLowerEqual, value, other, msgArgs...)
+	if !ok {
+		a.addError(err)
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotLowerEqual, value, max), msgArgs...)
-	return false
+	return ok
 }
 
 // Email returns true if a given value is a valid email format. It allows local
 // portion to be quoted text and ipv4 for the domain portion (between square brackets).
 func (a *Assertion) Email(value string, msgArgs ...interface{}) bool {
 	if !regexpEmail.MatchString(value) {
-		a.appendError(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
+		a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
 		return false
 	}
 
 	splits := strings.Split(value, "@")
 	domain := splits[len(splits)-1]
 	if len(domain) > 255 {
-		a.appendError(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
+		a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
 		return false
 	}
 
 	if regexpIpv4.MatchString(domain) {
-		a.appendError(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
+		a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "email"), msgArgs...)
 		return false
 	}
 
@@ -220,7 +306,7 @@ func (a *Assertion) Ipv4(value string, msgArgs ...interface{}) bool {
 		return true
 	}
 
-	a.appendError(fmt.Sprintf(errMsgNotValid, value, "ipv4"), msgArgs...)
+	a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "ipv4"), msgArgs...)
 	return false
 }
 
@@ -228,7 +314,7 @@ func (a *Assertion) Ipv4(value string, msgArgs ...interface{}) bool {
 func (a *Assertion) Alfanum(value string, msgArgs ...interface{}) bool {
 	for _, r := range value {
 		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
-			a.appendError(fmt.Sprintf(errMsgNot, value, "alfa-numeric"), msgArgs...)
+			a.addErrorMsg(fmt.Sprintf(errMsgNot, value, "alfa-numeric"), msgArgs...)
 			return false
 		}
 	}
@@ -240,7 +326,7 @@ func (a *Assertion) Alfanum(value string, msgArgs ...interface{}) bool {
 func (a *Assertion) Base64(value string, msgArgs ...interface{}) bool {
 	_, err := base64.StdEncoding.DecodeString(value)
 	if err != nil {
-		a.appendError(fmt.Sprintf(errMsgNotValid, value, "base64 encoded value"), msgArgs...)
+		a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "base64 encoded value"), msgArgs...)
 		return false
 	}
 	return true
@@ -249,7 +335,7 @@ func (a *Assertion) Base64(value string, msgArgs ...interface{}) bool {
 // Phone returns true if a given value ia a valid e164 phone number
 func (a *Assertion) Phone(value string, msgArgs ...interface{}) bool {
 	if !regexpE164.MatchString(value) {
-		a.appendError(fmt.Sprintf(errMsgNotValid, value, "phone"), msgArgs...)
+		a.addErrorMsg(fmt.Sprintf(errMsgNotValid, value, "phone"), msgArgs...)
 		return false
 	}
 
